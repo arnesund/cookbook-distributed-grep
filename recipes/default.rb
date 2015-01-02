6f96c53617014845b81dbf14a719a2826f2a0084
Arne Sund <arnesund@users.noreply.github.com>
2015-01-02 15:32:46 +0000

    Default recipe updated (WIP)

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,16 +11,29 @@
 include_recipe 'apt'
 include_recipe 'apache2'
 include_recipe 'sqlite'
-include_recipe 'php'
+#include_recipe 'php'
 include_recipe 'python'
 
-# PHP module dependencies
+# PHP dependency
+package "php5" do
+    action :install
+end
 package "php5-curl" do
-  action :install
+    action :install
 end
 package "php5-ldap" do
-  action :install
+    action :install
 end
 package "php5-sqlite" do
-  action :install
+    action :install
+end
+
+# Enable Apache2 PHP5 module
+#apache_module "php5" do
+#    filename "libphp5.so"
+#end
+
+# Apache2 setup
+apache_site "default" do
+    enable true
 end

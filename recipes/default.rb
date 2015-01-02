f661806b161dc96ab93a0c7284d67eef75921d76
Arne Sund <arnesund@users.noreply.github.com>
2015-01-02 14:46:29 +0000

    Install additional PHP packages

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,8 +7,20 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Dependencies
 include_recipe 'apt'
 include_recipe 'apache2'
 include_recipe 'sqlite'
 include_recipe 'php'
 include_recipe 'python'
+
+# PHP module dependencies
+package "php5-curl" do
+  action :install
+end
+package "php5-ldap" do
+  action :install
+end
+package "php5-sqlite" do
+  action :install
+end

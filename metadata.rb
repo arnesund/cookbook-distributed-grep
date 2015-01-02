5b57028aacbaf472b7af1a5828bb0e6362c60e0e
Arne Sund <arnes@met.no>
2015-01-02 09:46:15 +0000

    Default cookbook contents

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'distributed-grep'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures distributed-grep'
+long_description 'Installs/Configures distributed-grep'
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: distributed-grep
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
